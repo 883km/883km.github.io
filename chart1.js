1b7ef57be71815cbf2b69757195a1b1ce3e44cf0
SiCheng Meng <km883@outlook.com>
2023-07-30 18:59:06 -0400

    Initial commit

--- chart1.js
+++ chart1.js
@@ -1,168 +1,168 @@
 async function initLineRegion() {
   // PARSE DATA
-    const chartContainer = document.getElementById('chart1');
-    chartContainer.innerHTML = '';
-    
-    dataset = await d3.csv('https://883km.github.io/dataset.csv');
-    dataset.forEach(d => {
-        d.year = +d.year; // Using unary plus operator to convert to number
-        d.gdp = +d.gdp;
-        d.primary_energy_consumption_per_capita = +d.primary_energy_consumption_per_capita;
-      });
+  const chartContainer = document.getElementById('chart1');
+  chartContainer.innerHTML = '';
 
-    data = dataset.filter(function(d) {
-        return d.entity_type == "region";
-    });
+  dataset = await d3.csv('https://883km.github.io/dataset.csv');
+  dataset.forEach(d => {
+    d.year = +d.year; // Using unary plus operator to convert to number
+    d.gdp = +d.gdp;
+    d.primary_energy_consumption_per_capita = +d.primary_energy_consumption_per_capita;
+  });
 
-    multipleLine(data)
-  }
+  data = dataset.filter(function (d) {
+    return d.entity_type == "region";
+  });
 
-  async function initLineEco() {
-    // PARSE DATA
-      const chartContainer = document.getElementById('chart1');
-      chartContainer.innerHTML = '';
+  multipleLine(data)
+}
 
-      dataset = await d3.csv('https://883km.github.io/dataset.csv');
-      dataset.forEach(d => {
-          d.year = +d.year; // Using unary plus operator to convert to number
-          d.gdp = +d.gdp;
-          d.primary_energy_consumption_per_capita = +d.primary_energy_consumption_per_capita;
-        });
-  
-      data = dataset.filter(function(d) {
-          return d.entity_type == "eco_region";
-      });
-  
-      multipleLine(data)
-    }
-    
+async function initLineEco() {
+  // PARSE DATA
+  const chartContainer = document.getElementById('chart1');
+  chartContainer.innerHTML = '';
+
+  dataset = await d3.csv('https://883km.github.io/dataset.csv');
+  dataset.forEach(d => {
+    d.year = +d.year; // Using unary plus operator to convert to number
+    d.gdp = +d.gdp;
+    d.primary_energy_consumption_per_capita = +d.primary_energy_consumption_per_capita;
+  });
+
+  data = dataset.filter(function (d) {
+    return d.entity_type == "eco_region";
+  });
+
+  multipleLine(data)
+}
+
 function multipleLine(data) {
   // GROUP DATA
-    const grouped_data = Array.from(d3.group(data, (d) => d.entity), ([key, values]) => ({
-      key,
-      values,
-    }))
-    console.log(grouped_data)
-    console.log(grouped_data.map(d => d.key))
+  const grouped_data = Array.from(d3.group(data, (d) => d.entity), ([key, values]) => ({
+    key,
+    values,
+  }))
+  console.log(grouped_data)
+  console.log(grouped_data.map(d => d.key))
 
   //SVG  
-    // Set dimensions and margins for the chart
-    const margin = {top: 50, right: 140, bottom: 50, left: 100};
-    const width = 1000 - margin.left - margin.right;
-    const height = 600 - margin.top - margin.bottom;
-    
-    // Create the SVG element and append it to the chart container
-    const svg = d3.select("#chart1")
-      .append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-        .attr("transform", `translate(${margin.left},${margin.top})`);
-  
+  // Set dimensions and margins for the chart
+  const margin = { top: 50, right: 140, bottom: 50, left: 100 };
+  const width = 1000 - margin.left - margin.right;
+  const height = 600 - margin.top - margin.bottom;
+
+  // Create the SVG element and append it to the chart container
+  const svg = d3.select("#chart1")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", `translate(${margin.left},${margin.top})`);
+
 
   // SCALE AND AXIS
-    // Set up the x and y scales, color scales and domains
-    const xs = d3.scaleLinear()
-      .range([0, width])
-      .domain(d3.extent(data, d => d.year));
-    
-    const ys = d3.scaleLinear()
-      .range([height, 0])
-      .domain([0, d3.max(data, d => d.primary_energy_consumption_per_capita) + 5000]);
+  // Set up the x and y scales, color scales and domains
+  const xs = d3.scaleLinear()
+    .range([0, width])
+    .domain(d3.extent(data, d => d.year));
 
-    const cs = d3.scaleOrdinal()
-      .domain(grouped_data.map(d => d.key))
-      .range(d3.schemeTableau10);
-    console.log(cs)
-    
-    // Add the x & y axis
-    svg.append("g")
-      .attr("transform", `translate(0,${height})`)
-      .call(d3.axisBottom(xs)); 
+  const ys = d3.scaleLinear()
+    .range([height, 0])
+    .domain([0, d3.max(data, d => d.primary_energy_consumption_per_capita) + 5000]);
 
-    svg.append("g")
-      .call(d3.axisLeft(ys)); // TODO: ticks?
+  const cs = d3.scaleOrdinal()
+    .domain(grouped_data.map(d => d.key))
+    .range(d3.schemeTableau10);
+  console.log(cs)
 
-    // Add x-axis label "Year"
-    svg.append("text")
-      .attr("x", width / 2)
-      .attr("y", height + margin.top)
-      .style("text-anchor", "middle")
-      .style("font-size", 13)
-      .text("Year");
+  // Add the x & y axis
+  svg.append("g")
+    .attr("transform", `translate(0,${height})`)
+    .call(d3.axisBottom(xs));
 
-    // Add y-axis label "Energy Use Per Person kWh" and rotate it
-    svg.append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("x", 0 - height / 2)
-      .attr("y", -margin.left + 15)
-      .attr("dy", "1em")
-      .style("text-anchor", "middle")
-      .style("font-size", 13)
-      .text("Energy Use Per Person (kWh)");
-   
+  svg.append("g")
+    .call(d3.axisLeft(ys)); // TODO: ticks?
+
+  // Add x-axis label "Year"
+  svg.append("text")
+    .attr("x", width / 2)
+    .attr("y", height + margin.top)
+    .style("text-anchor", "middle")
+    .style("font-size", 13)
+    .text("Year");
+
+  // Add y-axis label "Energy Use Per Person kWh" and rotate it
+  svg.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("x", 0 - height / 2)
+    .attr("y", -margin.left + 15)
+    .attr("dy", "1em")
+    .style("text-anchor", "middle")
+    .style("font-size", 13)
+    .text("Energy Use Per Person (kWh)");
+
 
   // DRAW LINES    
-    // Create the line generator
-    const line = d3.line()
-      .x(d => xs(d.year))
-      .y(d => ys(d.primary_energy_consumption_per_capita));
+  // Create the line generator
+  const line = d3.line()
+    .x(d => xs(d.year))
+    .y(d => ys(d.primary_energy_consumption_per_capita));
 
-    // Add the line paths to the SVG element
-    svg.selectAll('.line')
-      .data(grouped_data)
-      .enter()
-      .append('g')
-      .append('path')
-      .attr('class', 'line')
-      .attr('fill', 'none')
-      .attr('stroke', d => cs(d.key))
-      .attr('stroke-width', 3)
-      .attr('d', d => line(d.values));
+  // Add the line paths to the SVG element
+  svg.selectAll('.line')
+    .data(grouped_data)
+    .enter()
+    .append('g')
+    .append('path')
+    .attr('class', 'line')
+    .attr('fill', 'none')
+    .attr('stroke', d => cs(d.key))
+    .attr('stroke-width', 3)
+    .attr('d', d => line(d.values));
 
-    // svg.selectAll('circle')
-    //   .data(grouped_data)
-    //   .enter()
-    //   .append('g')
-    //   .selectAll('circle')
-    //   .data(d => d.values)
-    //   .enter()
-    //   .append('circle')
-    //   .attr('cx', d => xs(d.year))
-    //   .attr('cy', d => ys(d.primary_energy_consumption_per_capita))
-    //   .attr('r', 3)
-    //   .attr('fill', d => cs(d.entity))
-    
+  // svg.selectAll('circle')
+  //   .data(grouped_data)
+  //   .enter()
+  //   .append('g')
+  //   .selectAll('circle')
+  //   .data(d => d.values)
+  //   .enter()
+  //   .append('circle')
+  //   .attr('cx', d => xs(d.year))
+  //   .attr('cy', d => ys(d.primary_energy_consumption_per_capita))
+  //   .attr('r', 3)
+  //   .attr('fill', d => cs(d.entity))
+
 
   // COLOR LEGEND & TITLE
-    //append legends
-    const legend = d3.select('svg')
-      .selectAll('g.legend')
-      .data(grouped_data)
-      .enter()
-      .append('g')
+  //append legends
+  const legend = d3.select('svg')
+    .selectAll('g.legend')
+    .data(grouped_data)
+    .enter()
+    .append('g')
 
-      .attr('class', 'legend');
+    .attr('class', 'legend');
 
-    legend.append('circle')
-      .attr('cx', width + margin.left + 20)
-      .attr('cy', (d, i) => i * 35 + margin.top + 5)
-      .attr('r', 5)
-      .style('fill', d => cs(d.key))
+  legend.append('circle')
+    .attr('cx', width + margin.left + 20)
+    .attr('cy', (d, i) => i * 35 + margin.top + 5)
+    .attr('r', 5)
+    .style('fill', d => cs(d.key))
 
-    legend.append('text')
+  legend.append('text')
     .attr('x', width + margin.left + 27)
     .attr('y', (d, i) => i * 35 + margin.top + 9)
     .text(d => d.key)
     .style("font-size", 12)
 
-    //append title
-    d3.select("svg")
+  //append title
+  d3.select("svg")
     .append("text")
     .attr("x", 485)
     .attr("y", 30)
     .attr("text-anchor", "middle")
-    .text("Energy Use Per Person, 1965 - 2022")
+    .text("Primary Energy Consumption Per Capita, 1965 - 2022")
     .style("fill", "black")
     .style("font-size", 20)
     .style("font-family", "Arial Black")
--- chart2.js
+++ chart2.js
@@ -1,148 +1,128 @@
-async function oneLine() {
+async function initOneLine() {
+    // PARSE DATA
+    //   const chartContainer = document.getElementById('chart2');
+    //   chartContainer.innerHTML = '';
+
     dataset = await d3.csv('https://883km.github.io/dataset.csv');
     dataset.forEach(d => {
         d.year = +d.year; // Using unary plus operator to convert to number
         d.gdp = +d.gdp;
         d.primary_energy_consumption_per_capita = +d.primary_energy_consumption_per_capita;
-      });
-  
-    data = dataset.filter(function(d) {
-        return d.entity_type == "country";
-      });
+    });
 
-  // GET COUNTRY
+    data = dataset.filter(function (d) {
+        return d.entity_type == "country";
+    });
+
+    // GET COUNTRY
+    //Populate the dropdown menu with country options
     const entities = Array.from(new Set(data.map(d => d.entity)));
-    console.log(entities)
     d3.select("#select-country")
         .selectAll('country-options')
         .data(entities)
         .enter()
         .append('option')
-        .text(function(d) {
+        .text(function (d) {
             return d;
         }) // text showed in the menu
-        .attr("value", function(d) {
+        .attr("value", function (d) {
             return d;
         }) // corresponding value returned by the button
+    
+    // Set up the initial line chart with the first country in the dropdown menu
+    const initialCountry = entities[0];
+    const filteredData = data.filter(d => d.entity === initialCountry);
+    updateLineChart(filteredData);
 
-  // SVG  
+    // Set up event listener for the dropdown: filter data by selected country
+    d3.select("#select-country").on("change", function () {
+        const selectedCountry = this.value;
+        const filteredData = data.filter(d => d.entity === selectedCountry);
+        updateLineChart(filteredData);
+    });
+}
+
+
+function updateLineChart(data) {
+    const chartContainer = document.getElementById('chart2');
+    chartContainer.innerHTML = '';
+
+    console.log("UpdateLineChart called with data: ", data);
+    //SVG  
     // Set dimensions and margins for the chart
-    const margin = {top: 50, right: 140, bottom: 50, left: 100};
+    const margin = { top: 50, right: 140, bottom: 50, left: 100 };
     const width = 1000 - margin.left - margin.right;
     const height = 600 - margin.top - margin.bottom;
-    
+
     // Create the SVG element and append it to the chart container
     const svg = d3.select("#chart2")
-      .append("svg")
+        .append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-        .attr("transform", `translate(${margin.left},${margin.top})`)
+        .append("g")
+        .attr("transform", `translate(${margin.left},${margin.top})`);
 
-  // SCALE AND AXIS
-    // Set up the x and y scales, color scales and domains /////////COLOR?
+
+    // SCALE AND AXIS
+    // Set up the x and y scales and domains
     const xs = d3.scaleLinear()
-      .range([0, width])
-      .domain(d3.extent(data, d => d.year));
-    
+        .range([0, width])
+        .domain([1965, 2022])
+    //.domain(d3.extent(data, d => d.year));
+
     const ys = d3.scaleLinear()
-      .range([height, 0])
-      .domain([0, d3.max(data, d => d.primary_energy_consumption_per_capita) + 5000]);
+        .range([height, 0])
+        .domain([0, d3.max(data, d => d.primary_energy_consumption_per_capita) + 5000]);
 
     // Add the x & y axis
     svg.append("g")
-      .attr("transform", `translate(0,${height})`)
-      .call(d3.axisBottom(xs)); 
-
+        .attr("transform", `translate(0,${height})`)
+        .call(d3.axisBottom(xs));
     svg.append("g")
-      .call(d3.axisLeft(ys));
+        .call(d3.axisLeft(ys));
 
     // Add x-axis label "Year"
     svg.append("text")
-      .attr("x", width / 2)
-      .attr("y", height + margin.top)
-      .style("text-anchor", "middle")
-      .style("font-size", 13)
-      .text("Year");
+        .attr("x", width / 2)
+        .attr("y", height + margin.top)
+        .style("text-anchor", "middle")
+        .style("font-size", 13)
+        .text("Year");
 
     // Add y-axis label "Energy Use Per Person kWh" and rotate it
     svg.append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("x", 0 - height / 2)
-      .attr("y", -margin.left + 15)
-      .attr("dy", "1em")
-      .style("text-anchor", "middle")
-      .style("font-size", 13)
-      .text("Energy Use Per Person (kWh)");
+        .attr("transform", "rotate(-90)")
+        .attr("x", 0 - height / 2)
+        .attr("y", -margin.left + 15)
+        .attr("dy", "1em")
+        .style("text-anchor", "middle")
+        .style("font-size", 13)
+        .text("Energy Use Per Person (kWh)");
+
+    //append title
+    d3.select("svg")
+        .append("text")
+        .attr("x", 485)
+        .attr("y", 30)
+        .attr("text-anchor", "middle")
+        .text("Primary Energy Consumption Per Capita by County, 1965 - 2022")
+        .style("fill", "black")
+        .style("font-size", 20)
+        .style("font-family", "Arial Black")
 
 
-  // DRAW LINES
-    // Initialize line with group 'world'
-    const worldData = data.filter(function(d) {
-        return d.entity == 'World'
-    });
-    const line = svg
-        .append('g')
-        .append("path")
-        .attr("id", "line-" + entities[0])
-        .data(worldData)
-        .attr("d", d3.line()
-            .x(function(d) {
-                return x(d.year)
-            })
-            .y(function(d) {
-                return y(d.primary_energy_consumption_per_capita)
-            })
-        )
-        .attr("stroke", 'blue')
-        .style("stroke-width", 4)
-        .style("fill", "none")
-    const mostRecentFirstCountryData = worldData[worldData.length - 1]
-    renderThirdChartAnnotations(mostRecentFirstCountryData, x(Number(mostRecentFirstCountryData.year)) - 10, y(Number(mostRecentFirstCountryData.productivity)) - 10, margin);
-  }
-  
-  //////////////////////////////////////////////////////////////////////////////////
-  async function renderThirdChart() {
-  
+    // DRAW LINES    
+    // Create the line generator
+    const line = d3.line()
+        .x(d => xs(d.year))
+        .y(d => ys(d.primary_energy_consumption_per_capita));
 
-  
-    function update(selectedGroup) {
-        // Create new data with the selection?
-        const countryData = filteredData.filter(function(d) {
-            return d.entity === selectedGroup;
-        });
-  
-        // Give these new data to update line
-        line
-            .datum(countryData)
-            .transition()
-            .duration(1000)
-            .attr("id", "line-" + selectedGroup)
-            .attr("d", d3.line()
-                .x(function(d) {
-                    return x(Number(d.year))
-                })
-                .y(function(d) {
-                    return y(Number(d.productivity))
-                })
-            )
-            .attr("stroke", function(d) {
-                return myColor(selectedGroup)
-            })
-  
-        // update the annotation
-        const finalCountryData = countryData[countryData.length - 1];
-        renderThirdChartAnnotations(finalCountryData, x(Number(finalCountryData.year)) - 10, y(finalCountryData.productivity) - 10, margin)
-    }
-  
-    // When the button is changed, run the updateChart function
-    d3.select("#select-country").on("change", function(d) {
-        // recover the option that has been chosen
-        const selectedOption = d3.select(this).property("value")
-        // run the updateChart function with this selected option
-        update(selectedOption)
-  
-    })
-  
-  }
-  
+    // Add the line paths to the SVG element
+    svg.append('path')
+        .attr('class', 'line')
+        .attr('fill', 'none')
+        .attr('stroke', 'steelblue')
+        .attr('stroke-width', 3)
+        .attr('d', line(data));
+
+}
